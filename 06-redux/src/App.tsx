5b984bc14e8b4ab4cb6b5d6db9432f33e1bbb248
Igor Silva <danieligord7@hotmail.com>
2022-12-10 20:52:59 -0300

    SECTION 6: Redux - Creating Card Component

--- 06-redux/src/App.tsx
+++ 06-redux/src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./Components/Card";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Exercício React-Redux</Title>
+      <Row>
+        <Card title="CARD #01" headColor="#472121" bodyColor="#580903">
+          Card
+        </Card>
+
+        <Card title="CARD #01" headColor="#1d4225" bodyColor="#224e2b">
+          Card
+        </Card>
+      </Row>
+
+      <Row>
+        <Card title="CARD #02" headColor="#050f31" bodyColor="#19223f">
+          Card
+        </Card>
+
+        <Card title="CARD #02" headColor="#36051e" bodyColor="#491932">
+          Card
+        </Card>
+      </Row>
+    </Container>
   );
 }
 
---
+++ 06-redux/src/Components/Card.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+interface CardProps {
+    children: JSX.Element | string
+    title: string
+    headColor: string
+    bodyColor: string
+}
+
+const CardContainer = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+`;
+
+const CardHeader = styled.div`
+    display: flex;  
+    justify-content: center;
+    color: #FFF;
+    background-color: ${props => props.color ?? 'black'};
+`;
+
+const CardTitle = styled.h2`
+    padding: 5px;
+    flex: 1;
+`;
+
+const CardContent = styled.div`
+    padding: 20px;
+    background-color: ${props => props.color ?? 'black'};
+`;
+
+
+const Card=(props: CardProps)=>{
+    return(
+        <CardContainer>
+            <CardHeader color={props.headColor}>
+                <CardTitle>{props.title}</CardTitle>
+            </CardHeader>
+            <CardContent color={props.bodyColor}>
+                {props.children}
+            </CardContent>
+        </CardContainer>
+    )
+}
+
+export default Card
